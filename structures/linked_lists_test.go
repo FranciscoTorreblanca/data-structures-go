3dd9c756cbbd591fdfe98407fad070a135511fee
Francisco Torreblanca <francisco@apli.jobs>
2020-09-11 02:39:28 -0500

    Add tests for queue.

--- structures/linked_lists_test.go
+++ structures/linked_lists_test.go
@@ -5,27 +5,27 @@ import (
 )
 
 const (
-	v1 = 1
-	v2 = "2"
-	v3 = false
+	lv1 = 1
+	lv2 = "2"
+	lv3 = false
 )
 
 func TestLinkedList(t *testing.T) {
-	ll := NewLinkedList(v1)
-	if ll.root.value != v1 {
-		t.Errorf("NewLinkedList(%v): %v; want %v", v1, ll.root.value, v1)
+	ll := NewLinkedList(lv1)
+	if ll.root.value != lv1 {
+		t.Errorf("NewLinkedList(%v): %v; want %v", lv1, ll.root.value, lv1)
 	}
-	if fn := ll.First(); fn.value != v1 {
-		t.Errorf("First(): %v; want %v", fn.value, v1)
+	if fn := ll.First(); fn.value != lv1 {
+		t.Errorf("First(): %v; want %v", fn.value, lv1)
 	}
 
-	ll.Add(v2)
-	ll.Add(v3)
-	if nn := ll.root.Next(); nn.value != v2 {
-		t.Errorf("Next(): %v; want %v", nn.value, v2)
+	ll.Add(lv2)
+	ll.Add(lv3)
+	if nn := ll.root.Next(); nn.value != lv2 {
+		t.Errorf("Next(): %v; want %v", nn.value, lv2)
 	}
-	if ln := ll.Last(); ln.value != v3 {
-		t.Errorf("Last(): %v; want %v", ln.value, v3)
+	if ln := ll.Last(); ln.value != lv3 {
+		t.Errorf("Last(): %v; want %v", ln.value, lv3)
 	}
 	if l := ll.Length(); l != 3 {
 		t.Errorf("Lenght(): %v; want %v", l, 3)
---
+++ structures/queue_test.go
@@ -0,0 +1,35 @@
+package structures
+
+import (
+	"testing"
+)
+
+const (
+	qv1 = "1"
+	qv2 = 2
+	qv3 = true
+)
+
+func TestQueue(t *testing.T) {
+	q := NewQueue(qv1)
+	if q.items[0] != qv1 {
+		t.Errorf("NewQueue(%v): %v; want %v", qv1, q.items[0], qv1)
+	}
+	if fi := q.First(); fi != qv1 {
+		t.Errorf("First(): %v; want %v", fi, qv1)
+	}
+
+	q.Add(qv2)
+	q.Add(qv3)
+	if li := q.Last(); li != qv3 {
+		t.Errorf("Last(): %v; want %v", li, qv3)
+	}
+	if l := q.Length(); l != 3 {
+		t.Errorf("Lenght(): %v; want %v", l, 3)
+	}
+	q.Remove()
+
+	if l := q.Length(); l != 2 {
+		t.Errorf("Length(): %v; want %v", l, 2)
+	}
+}
